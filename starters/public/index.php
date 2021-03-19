9bd19574a06c3da058906ed30b3260cfba99e1ca
Anass-sudo <anasstfm@gmail.com>
2021-03-19 16:38:22 +0100

    All done

--- starters/public/index.php
+++ starters/public/index.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    <link href="./asset/css/style.css" rel="stylesheet">
-
+    <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="./asset/css/style.css" rel="stylesheet"> -->
     <title>Booking website &mdash; First-app</title>
 </head>
 <body>
